060f3305375bcc4c8b71a3f8e3288493eb2a8a62
MatthijsB1603 <157570@student.horizoncollege.nl>
2022-06-16 12:45:18 +0200

    Initial commit

---
+++ STAGE/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        CopyFile();   
+    }
+
+    public static void CopyFile()
+    {
+        //Finds directory with the images
+
+        string[] filePath = Directory.GetFiles(@"C:\Users\mlbud\OneDrive\Documenten\STAGEOPDRACHT\MAP1");
+        //Foreach loop goes through every file in the directory
+        foreach (var fileName in filePath)
+        {
+            Path.GetFileNameWithoutExtension(fileName);
+            var bestandNaam = Path.GetFileName(fileName);
+
+            string file = fileName.ToString();
+            //fileDest means file destination
+            string fileDest = @"C:\Users\mlbud\OneDrive\Documenten\STAGEOPDRACHT\MAP2" + bestandNaam;
+
+            string copyFolderFrom = filePath + file;
+            string copyFolderTo = fileDest + file;
+            {
+                File.Copy(copyFolderFrom, copyFolderTo, true);
+            }
+        }
+    }
+}
