051e3124665cdae5db6464af548fc6ef580fb1e5
MatthijsB1603 <157570@student.horizoncollege.nl>
2022-06-16 13:18:55 +0200

    Finished stage opdracht
    Function: Copies all files from a folder and pastes them in another

--- STAGE/Program.cs
+++ STAGE/Program.cs
@@ -11,22 +11,22 @@ public class Program
     public static void CopyFile()
     {
         //Finds directory with the images
+        string filep = @"C:\Gebruikers\mlbud\OneDrive\Documenten\STAGEOPDRACHT\MAP1";
 
-        string[] filePath = Directory.GetFiles(@"C:\Users\mlbud\OneDrive\Documenten\STAGEOPDRACHT\MAP1");
+        string[] filePath = Directory.GetFiles(@"C:\Users\mlbud\OneDrive\Documenten\STAGEOPDRACHT\MAP1\");
+        Console.WriteLine("Found directory");
+
         //Foreach loop goes through every file in the directory
         foreach (var fileName in filePath)
-        {
-            Path.GetFileNameWithoutExtension(fileName);
+        {            
             var bestandNaam = Path.GetFileName(fileName);
-
             string file = fileName.ToString();
-            //fileDest means file destination.
-            string fileDest = @"C:\Users\mlbud\OneDrive\Documenten\STAGEOPDRACHT\MAP2" + bestandNaam;
+            string fileDest = @"C:\Users\mlbud\OneDrive\Documenten\STAGEOPDRACHT\MAP2\" + bestandNaam;
 
-            string copyFolderFrom = filePath + file;
-            string copyFolderTo = fileDest + file;
+            string copyFolderFrom = fileName;
             {
-                File.Copy(copyFolderFrom, copyFolderTo, true);
+                File.Copy(copyFolderFrom, fileDest, true);
+                Console.WriteLine("Copied file to folder");
             }
         }
     }
