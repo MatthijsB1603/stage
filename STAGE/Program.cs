b77f2f309065a2f19267b7cf93fd9175f579cbff
MatthijsB1603 <157570@student.horizoncollege.nl>
2022-08-30 10:03:28 +0200

    grab

--- STAGE/Program.cs
+++ STAGE/Program.cs
@@ -1,24 +1,25 @@
-﻿using System;
+﻿using Excel = Microsoft.Office.Interop.Excel;
+using System;
 using System.IO;
 
 public class Program
 {
-    static void Main(string[] args)
+    private static void Main(string[] args)
     {
-        CopyFile();   
+        CopyFile();
+        WriteCopy();
     }
+    //GUI to initialize CopyFile() and WriteCopy().
+
 
     public static void CopyFile()
     {
-        //Finds directory with the images
-        string filep = @"C:\Gebruikers\mlbud\OneDrive\Documenten\STAGEOPDRACHT\MAP1";
-
         string[] filePath = Directory.GetFiles(@"C:\Users\mlbud\OneDrive\Documenten\STAGEOPDRACHT\MAP1\");
         Console.WriteLine("Found directory");
 
-        //Foreach loop goes through every file in the directory
+        //Foreach loop goes through every file in the directory 
         foreach (var fileName in filePath)
-        {            
+        {
             var bestandNaam = Path.GetFileName(fileName);
             string file = fileName.ToString();
             string fileDest = @"C:\Users\mlbud\OneDrive\Documenten\STAGEOPDRACHT\MAP2\" + bestandNaam;
@@ -30,4 +31,32 @@ public class Program
             }
         }
     }
-}
+
+    //WriteCopy() now creates an xls file. only thing needed to be changed is what is being written
+    public static void WriteCopy()
+    {
+
+
+        Excel.Application App = new Excel.Application();
+        if (App != null)
+        {
+            Excel.Workbook Workbook = App.Workbooks.Add();
+            Excel.Worksheet Worksheet = (Excel.Worksheet)Workbook.Sheets.Add();
+
+            string[] filePath = Directory.GetFiles(@"C:\Users\mlbud\OneDrive\Documenten\STAGEOPDRACHT\MAP2");
+            var fileNames = Path.GetFileNameWithoutExtension(@"C:\Users\mlbud\OneDrive\Documenten\STAGEOPDRACHT\MAP2");
+            var i = 1;
+
+            foreach (var fileName in filePath)
+                {
+                    Worksheet.Cells[i++, 2] = fileName;
+                    i++;
+                }
+
+                App.ActiveWorkbook.SaveAs2(@"C:\Users\mlbud\OneDrive\Documenten\STAGEOPDRACHT\pollos.xls");
+
+                Workbook.Close();
+                App.Quit();
+            }
+        }
+    }
